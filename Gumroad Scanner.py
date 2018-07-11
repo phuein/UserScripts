c7a7bd9ed27a36a30af01451c743c32a4afa0897
James Koss <phuein@gmail.com>
2018-07-11 00:03:29 -0400

    A small useful scanner for Gumroad items.
    I wanted to quickly see all the free items they got for my search query, so I made this. Python 3.

---
+++ Gumroad Scanner.py
@@ -0,0 +1,147 @@
+import requests
+from urllib.parse import quote
+import time, codecs, re, random, signal, sys
+
+DEBUG = 0
+
+domain = 'https://gumroad.com'
+
+# Ctrl+C stops page fetching loop,
+# while saving results.
+interrupted = False
+def signal_handler(sig, frame):
+    global interrupted
+    print('Interrupted! Finishing...')
+    interrupted = True
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+# Get user settings.
+query = ''
+while not query:
+    query = input('Enter search query: ')
+
+maxResults = input('Max result pages [1]: ')
+maxResults = int(maxResults) if maxResults else 1
+
+maxPrice = input('Max price per item [0]: ')
+maxPrice = float(maxPrice) if maxPrice != '' else 0
+
+# Log results to a new file.
+def log(text, f='txt'):
+    t = time.strftime("%Y%m%d_%H%M%S")
+
+    with open('gumroad_results_'+t+'.'+f, 'w') as file:
+        file.write(text)
+
+# Return [(uniqueID str, price float), ..] from gumroad search results page.
+def scrape(page):
+    try:
+        r = requests.get(domain + page)
+    except Exception as err:
+        DEBUG and print('Failed to connect: '+str(err))
+        return []
+    
+    source = r.text
+    # print(source)
+
+    # data-unique-permalink=\"OJlw\"
+    n = 'data-unique-permalink=\\"'
+    # itemprop=\"price\"\u003e$149+\u003c
+    p = 'itemprop=\\"price\\"\\u003e'
+    # class=\"discover-product__information\"\u003e\n\u003ch2\u003e\nvoxel plugin™ PRO\n\u003c/h2
+    t = 'class=\\"discover-product__information\\"\\u003e\\n\\u003ch2\\u003e\\n'
+
+    res = []
+    lastIndex = 0
+    for i in range(8):
+        # Get item id for linking.
+        a = source.find(n, lastIndex)
+        # No more items to list, so finish.
+        if a == -1:
+            break
+        b = source.find('\\"', a+len(n))
+        name = source[a+len(n):b]
+        DEBUG and print(name)
+        # Get item price.
+        a = source.find(p, lastIndex)
+        b = source.find('\\u003c', a+len(p))
+        price = source[a+len(p):b]
+        DEBUG and print(price)
+        # Extract number only.
+        price = re.findall(r'\d+', price)
+        # BUG: No price available.
+        if not price:
+            break
+        price = price[0]
+        # Get item title.
+        a = source.find(t, lastIndex)
+        b = source.find('\\n', a+len(t))
+        title = source[a+len(t):b]
+        DEBUG and print(title)
+        # Add item as tuple.
+        res.append((name, float(price), title))
+        lastIndex = b
+    return res
+
+# Main loop.
+results = []
+pageCount = '1'
+def run():
+    for i in range(maxResults):
+        # Signal interruption from system or user.
+        if interrupted:
+            break
+
+        # Increments of 8, so 1, 9,.. and so on.
+        v = i * 9 if i > 1 else 1
+
+        link = '/discover_search?from=' + str(v) + '&query=' + quote(query)
+
+        # Imitate human delay.
+        if i > 1:
+            # 1-3 seconds.
+            z = random.randrange(10, 30) / 10
+            DEBUG and print('Page '+str(i)+' - Sleeping for '+str(z)+' seconds...')
+            time.sleep(z)
+
+        res = scrape(link)
+
+        # Finished early.
+        if not res:
+            DEBUG and print('No more results.')
+            break
+
+        pageCount = str(i)
+
+        # Get id's and prices for results in page.
+        # Save the ones below the maximum price limit.
+        for item in res:
+            uid, price, title = item
+            if price <= maxPrice:
+                x = {
+                    'id': uid,
+                    'price': price,
+                    'title': title,
+                }
+                # NOTE: Slow check, but keeps order.
+                if x not in results:
+                    results.append(x)
+
+def finish():
+    DEBUG and print(results)
+    # Log to file.
+    if not results:
+        text = 'No results for <b>'+query+'</b>.'
+    else:
+        text = ''
+        for item in results:
+            s = domain + '/l/' + item['id']
+            text += '<a href=' + s + ' target=_blank>'+item['title']+'<br>'
+    log(text, 'html')
+
+    input('Got '+str(len(results))+' matches saved from '+pageCount+' pages. Press Any Key To Quit...')
+
+# Calls.
+run()
+finish()
