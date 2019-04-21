662225e469dd2e5bd15a8a87e2ca3ebf3c7708f2
James Koss <phuein@gmail.com>
2019-04-21 15:51:29 -0400

    Create ESOLauncherKiller.py
    Kills the ESO launcher if the game is running. Checks every 5 minutes by default.

---
+++ ESOLauncherKiller.py
@@ -0,0 +1,15 @@
+import os
+from time import sleep
+
+while True:
+    foundGame = os.popen('tasklist /FI "IMAGENAME eq eso64.exe" 2>NUL | \
+        find /I /N "eso64.exe"').read()
+
+    foundLauncher = os.popen('tasklist /FI "IMAGENAME eq Bethesda.net_Launcher.exe" 2>NUL | \
+        find /I /N "Bethesda.net_Launcher.exe"').read()
+
+    if foundGame and foundLauncher:
+        # k = os.popen('taskkill /IM Bethesda.net_Launcher.exe').read()
+        os.popen('taskkill /IM Bethesda.net_Launcher.exe')
+
+    sleep(60 * 5)  # Five minutes.
