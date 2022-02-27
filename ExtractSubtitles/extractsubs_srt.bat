d2ee54fbeec67a03167583d1678f91bbb7b7d120
James Koss <phuein@gmail.com>
2022-02-27 17:24:14 -0500

    Update extractsubs_srt.bat

--- ExtractSubtitles/extractsubs_srt.bat
+++ ExtractSubtitles/extractsubs_srt.bat
@@ -4,7 +4,7 @@ REM ffmpeg -i "%~1" "%~n1.eng.srt"
 
 :LOOP
 if "%~1"=="" goto :END
-  ffmpeg -i "%~1" "%~n1.eng.srt"
+  ffmpeg -i "%~1" -map 0:m:language:eng "%~n1.eng.srt"
   shift
   goto :LOOP
 :END
